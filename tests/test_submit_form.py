366d82fc3fe51e8602da459f55da994733fcf312
iv.pimenov <ivan.pimenov@fabit.ru>
2022-06-28 00:38:02 +0700

    fixed test

--- tests/test_submit_form.py
+++ tests/test_submit_form.py
@@ -2,7 +2,7 @@ from selene import have
 from selene.support.shared import browser
 
 
-def test_submit_form(browser_management):
+def test_submit_form():
     browser.open('/automation-practice-form')
 
     browser.element('#firstName').type('enchik')
